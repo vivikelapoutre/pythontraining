211d51c026d217cbddb208f7c83bb907c53b35ae
Vivike Lapoutre <V.Lapoutre@gmail.com>
2026-06-02 18:55:59 +0200

    initial commit

---
+++ conversions.py
@@ -0,0 +1,19 @@
+FEET_PER_MILE = 5280
+
+
+def mile_to_feet(distance_in_miles: float):
+    return distance_in_miles * FEET_PER_MILE
+
+
+def convert(a):
+    return a / 1609344
+
+
+if __name__ == "__main__":
+    distance_in_km = 10
+
+    distance_in_miles = convert(distance_in_km)
+    print(f"{distance_in_km} km is {distance_in_miles} mijl")
+
+    distance_in_feet = mile_to_feet(distance_in_miles)
+    print(f"{distance_in_miles} mijl is {distance_in_feet} feet")
---
+++ tests.py
@@ -0,0 +1,11 @@
+from conversions import mile_to_feet, convert
+
+
+def test_function_converts_miles():
+    assert mile_to_feet(1) == 5280
+    assert mile_to_feet(0.1) == 528
+
+
+def test_function_converts_km():
+    assert int(convert(10)) == 6
+    assert int(convert(1000)) == 621
